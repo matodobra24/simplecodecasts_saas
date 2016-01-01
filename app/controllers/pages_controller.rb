d56c56a4958a1ff9b56067a38623bd0e32cf1346
Johnathan Matodobra <matodobra24@gmail.com>
2016-01-01 16:07:00 +0000

    Added params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
     
     def home
         
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
+        
     end
     
     def about
