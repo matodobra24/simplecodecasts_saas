bae67d72df69c3807972b60fab1de4239f15979d
Johnathan Matodobra <matodobra24@gmail.com>
2016-01-04 22:51:26 +0000

    Changes user.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
     function stripeResponseHandler(status, response) {
         // Get a reference to the form:
-        var l = $(".new_form");
+        var l = $("#pro_plan");
         
         if (response.error) {
             
